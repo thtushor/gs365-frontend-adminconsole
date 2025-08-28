bd5aeefcac96118b6cf5762a5c626cc318592f65
thtushor <hasankhanltd@gmail.com>
2025-08-28 11:37:22 +0600

    fix(api): correct BASE_URL to point to the correct server endpoint

--- src/api/ApiList.js
+++ src/api/ApiList.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://glorypos.com/gss-server";
+export const BASE_URL = "https://glorypos.com/gs-server";
 // export const BASE_URL = "http://localhost:3000";
 
 export const GAME_SITE = "https://gamestar365.com";
