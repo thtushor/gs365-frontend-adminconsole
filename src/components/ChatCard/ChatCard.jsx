a9717f87809589e971050ad6ef69e8e14ed7367e
thtushor <hasankhanltd@gmail.com>
2025-09-20 01:31:36 +0600

    style: Reposition active user indicator in ChatCard
    The active user status indicator (green circle) in the ChatCard component has been moved. Previously, it was positioned at the far right of the card. It is now rendered directly next to the user's name. This change improves the visual association of the active status with the user, enhancing clarity and user experience.

--- src/components/ChatCard/ChatCard.jsx
+++ src/components/ChatCard/ChatCard.jsx
@@ -5,21 +5,24 @@ const ChatCard = ({ name, message, time, avatar, isActive, isUserActive, onClick
 
   return (
     <div
-      className={`flex relative items-center hover:bg-[#01dc8423] duration-150 cursor-pointer gap-2 p-[10px] py-2 border-b border-[#01dc844e] ${
-        isActive ? "bg-[#01dc8423]" : ""
-      }`}
+      className={`flex relative items-center hover:bg-[#01dc8423] duration-150 cursor-pointer gap-2 p-[10px] py-2 border-b border-[#01dc844e] ${isActive ? "bg-[#01dc8423]" : ""
+        }`}
       onClick={onClick}
     >
       <img
         src={avatar}
         alt=""
-        className={`w-[40px] h-[40px] border-2 rounded-full ${
-          isActive ? "border-[#01dc84]" : "border-white/80"
-        }`}
+        className={`w-[40px] h-[40px] border-2 rounded-full ${isActive ? "border-[#01dc84]" : "border-white/80"
+          }`}
       />
       <div className="flex-1"> {/* Use flex-1 to make this div take available space */}
         <div className="flex justify-between items-center"> {/* Flex container for name and time */}
-          <p className={`${isActive ? "text-[#01dc84]" : "text-white/80"} font-medium`}>{name}</p>
+          <div>
+            <p className={`${isActive ? "text-[#01dc84]" : "text-white/80"} font-medium`}>{name}</p>
+            {isUserActive && (
+              <div className="w-[10px] h-[10px] rounded-full bg-[#01dc84] absolute right-3 top-1/2 -translate-y-1/2" />
+            )}
+          </div>
           {time && ( // Only show time if it exists
             <div className="text-[12px] text-white/90 italic">
               ({time})
@@ -30,9 +33,7 @@ const ChatCard = ({ name, message, time, avatar, isActive, isUserActive, onClick
           <span className={`${isActive ? "text-white" : "text-white/70"}`}>{displayMessage}</span>
         </div>
       </div>
-      {isUserActive && (
-        <div className="w-[10px] h-[10px] rounded-full bg-[#01dc84] absolute right-3 top-1/2 -translate-y-1/2" />
-      )}
+
     </div>
   );
 };
