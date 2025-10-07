277e3a6f580da36b7df78409c783c58c3c012125
thtushor <hasankhanltd@gmail.com>
2025-10-07 10:52:00 +0600

    add help center icons

--- src/components/HelpCenterIcon.jsx
+++ src/components/HelpCenterIcon.jsx
@@ -1,18 +1,24 @@
 import { BiMessage } from "react-icons/bi";
+import { useChat } from "../hooks/useChat";
 
 export const HelpCenterIconWithChatsCount = () => {
-  return (
-    <div className="-top-2 left-4 relative inline-block" title="You have new messages">
-      {/* Replace this div with your Help icon */}
-      {/* <div className="w-4 h-4 flex items-center justify-center">
+
+    const { messages } = useChat();
+
+    const unreadMessagesCount = messages?.filter(
+
+    return (
+        <div className="-top-2 left-4 relative inline-block" title="You have new messages">
+            {/* Replace this div with your Help icon */}
+            {/* <div className="w-4 h-4 flex items-center justify-center">
         <BiMessage size={10} />
       </div> */}
 
-      {/* Blinking red circle */}
-      <span className="absolute top-0 right-0 block w-3 h-3 rounded-full bg-red-600 animate-ping"></span>
+            {/* Blinking red circle */}
+            <span className="absolute top-0 right-0 block w-3 h-3 rounded-full bg-red-600 animate-ping"></span>
 
-      {/* Solid red dot (stays visible while ping animates) */}
-      <span className="absolute top-0 right-0 block w-3 h-3 rounded-full bg-red-600"></span>
-    </div>
-  );
+            {/* Solid red dot (stays visible while ping animates) */}
+            <span className="absolute top-0 right-0 block w-3 h-3 rounded-full bg-red-600"></span>
+        </div>
+    );
 };
